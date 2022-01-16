05eab02afd5bd04b8fe35c9b34ad3876d7460962
MTristan <53493044+MTristan012@users.noreply.github.com>
2022-01-16 13:35:23 -0600

    Delete Checkpoint 2.py

--- Checkpoint 2.py
+++
@@ -1,9 +0,0 @@
-"""
-By: Tristan Perea
-02 checkpoint
-"""
-
-Name = input("Hello! What is your name?: ")
-LastName = input("OK! Now... What is your last name?: ")
-print("Oh right! You are " + LastName + ", " + Name +" "+ LastName)
-print("Nice to meet you agent! " + LastName)
