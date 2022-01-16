d867bf2c8347d7cb83c2ac21574dd0e252928aa0
MTristan <53493044+MTristan012@users.noreply.github.com>
2022-01-16 13:31:58 -0600

    Add files via upload

---
+++ 02_Checkpoint.py
@@ -0,0 +1,9 @@
+"""
+By: Tristan Perea
+02 checkpoint
+"""
+
+Name = input("Hello! What is your name?: ")
+LastName = input("OK! Now... What is your last name?: ")
+print("Oh right! You are " + LastName + ", " + Name +" "+ LastName)
+print("Nice to meet you agent! " + LastName)
