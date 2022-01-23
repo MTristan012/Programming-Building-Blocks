4d314c5ad3cf22922c0f9a81a827fface5c5c9f3
MTristan <53493044+MTristan012@users.noreply.github.com>
2022-01-22 23:36:44 -0600

    Add files via upload

--- 05_Teach_Team_Activity.py
+++ 05_Teach_Team_Activity.py
@@ -5,15 +5,31 @@ By: Tristan Perea
 
 score = int(input("Insert your score here: "))
 
-if score <= 100 & score >= 90:
-    print("Your grade is A")
-elif score < 90 & score >= 80:
-    print("Your grade is B")
-elif score < 80 & score >= 70:
-    print("Your grade is C")
-elif score < 70 & score >= 60:
-    print("Your grade is D")
-elif score < 60:
-    print("Your grade is F")
+if score <= 100 and score >= 90:
+    grade = "A"
+elif score < 90 and score >= 80:
+    grade = "B"
+elif score < 80 and score >= 70:
+    grade = "C"
+elif score < 70 and score >= 60:
+    grade = "D"
 else:
-    print("score not valid")
+    grade = "F"
+
+if score <= 93 and score > 60:
+    scoreSign = score % 10
+    if scoreSign >= 7:
+        sign = "+"
+    elif scoreSign < 7 and scoreSign > 3:
+        sign = ""
+    elif scoreSign <= 3:
+        sign = "-"
+else:
+    sign = ""
+
+if score >= 60:
+    passMessage = " Congratulation you Pass!"
+else:
+    passMessage = " You Shall not Pass"
+
+print("Your Grade is " + grade + sign + passMessage)
