744d8e677ed01aa35c0bb7f2033956bc3573d867
MTristan <53493044+MTristan012@users.noreply.github.com>
2022-02-09 18:46:01 -0600

    Add files via upload

--- 06_Teach_Team_Activity.py
+++ 06_Teach_Team_Activity.py
@@ -3,24 +3,22 @@ By: Tristan Perea
 06 Team Activity
 """
 
-print("Welcome, please answer the next questions:")
-
-firstAgeRider = int(input("What is the age of the first rider? "))
-firstHeightRider = int(input("What is the height of the first rider? "))
-secondRider = input("Is there a second rider(yes/no)? ")
-
+firstAgeR = int(input("What is the age of the first rider? "))
+firstHeightR = int(input("What is the height of the first rider? "))
+secondRider = input("Is there a second rider (yes/no)? ")
 secondRider = secondRider.lower()
-
 if secondRider == "yes":
-    secondAgeRider = int(input("What is the age of the second rider? "))
-    secondHeightRider = int(input("What is the height of the second rider? "))
-elif secondRider == "no":
-    secondAgeRider = 0
-    secondHeightRider = 0
-
-if firstHeightRider >= 36 or secondHeightRider >= 36:
-    if (firstAgeRider >= 18 and firstHeightRider >= 62) or (secondAgeRider <= 18 and secondHeightRider >= 62):
-        print("Welcome to the ride. Please be safe and have fun!")
+   secondAgeR = int(input("What is the age of the second rider? "))
+   secondHeightR = int(input("What is the height of the second rider? "))
+   if firstHeightR >= 36 and secondHeightR >= 36:
+       if firstAgeR >= 18 or secondAgeR >= 18:
+           print("Welcome to the ride. Please be safe and have fun!")
+       else:
+           print("Sorry, you may not ride.")
+   else:
+       print("Sorry, you may not ride.")
 else:
-    print("Sorry, you may not ride.")
-    
+    if firstAgeR >= 18 and firstHeightR >= 62:
+        print("Welcome to the ride. Please be safe and have fun!")
+    else:
+        print("Sorry, you may not ride.")
