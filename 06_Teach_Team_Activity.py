a9da9f4ffbdb8cf820e6496dcc8b8cf61cff1ad1
MTristan <53493044+MTristan012@users.noreply.github.com>
2022-02-09 22:05:38 -0600

    Add files via upload

--- 06_Teach_Team_Activity.py
+++ 06_Teach_Team_Activity.py
@@ -8,17 +8,28 @@ firstHeightR = int(input("What is the height of the first rider? "))
 secondRider = input("Is there a second rider (yes/no)? ")
 secondRider = secondRider.lower()
 if secondRider == "yes":
-   secondAgeR = int(input("What is the age of the second rider? "))
-   secondHeightR = int(input("What is the height of the second rider? "))
-   if firstHeightR >= 36 and secondHeightR >= 36:
-       if firstAgeR >= 18 or secondAgeR >= 18:
+    secondAgeR = int(input("What is the age of the second rider? "))
+    secondHeightR = int(input("What is the height of the second rider? "))
+    if firstHeightR >= 36 and secondHeightR >= 36:
+        if firstAgeR >= 18 or secondAgeR >= 18:
            print("Welcome to the ride. Please be safe and have fun!")
-       else:
+        elif (firstAgeR >= 12 and firstHeightR >= 52) and (secondAgeR >= 12 and secondHeightR >= 52):
+            print("Welcome to the ride. Please be safe and have fun!")
+        elif (firstAgeR >= 16 and secondAgeR >= 14) or (firstAgeR >= 14 and secondAgeR >= 16):
+            print("Welcome to the ride. Please be safe and have fun!")
+        else:
            print("Sorry, you may not ride.")
-   else:
+    else:
        print("Sorry, you may not ride.")
 else:
     if firstAgeR >= 18 and firstHeightR >= 62:
         print("Welcome to the ride. Please be safe and have fun!")
+    elif (firstAgeR <= 17 and firstAgeR >= 12) and firstHeightR >= 36:
+        goldenPass = input("Do You have a Golden Pass? (yes/no)")
+        goldenPass = goldenPass.lower()
+        if goldenPass == "yes":
+            print("Welcome to the ride. Please be safe and have fun!")
+        else:
+          print("Sorry, you may not ride.")
     else:
         print("Sorry, you may not ride.")
